7b56b3f45fb6329891f86003adadbff66203cfcc
fnv21031 <greg.corbett@stfc.ac.uk>
2016-03-08 11:15:55 +0000

    Add STATIC_ROOT to settigns.py
    - allow apache to serve django rest framework pages

--- apel_rest/settings.py
+++ apel_rest/settings.py
@@ -80,6 +80,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = '/var/www/html/static'
 STATIC_URL = '/static/'
 
 # debugging stuff
