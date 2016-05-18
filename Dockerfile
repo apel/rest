7403eef83470d7fa2c9e196aa95adfd9aee5f2f1
Greg Corbett <greg.corbett@stfc.ac.uk>
2016-05-18 13:49:53 +0100

    Attempt to configure certificates on start up

--- Dockerfile
+++ Dockerfile
@@ -12,8 +12,8 @@ RUN rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noar
 RUN yum -y install python-pip python-devel python-ldap
 
 # install python installers
-RUN pip install pip --upgrade
-RUN pip install setuptools --upgrade
+# RUN pip install pip --upgrade
+# RUN pip install setuptools --upgrade
 
 # install mysql
 RUN yum -y install mysql-server mysql-devel gcc
@@ -60,3 +60,5 @@ RUN cp /var/www/html/conf/ssl.conf /etc/httpd/conf.d/ssl.conf
 # expose apache and SSL ports
 EXPOSE 80
 EXPOSE 443
+
+CMD /rest-start_docker_script/run_on_entry.sh
--- run_on_entry.sh
+++ run_on_entry.sh
@@ -4,10 +4,14 @@
 touch /etc/yum.repos.d/EGI-trustanchors.repo
 echo -e "# EGI Software Repository - REPO META (releaseId,repositoryId,repofileId) - (10824,-,2000)\n[EGI-trustanchors]\nname=EGI-trustanchors\nbaseurl=http://repository.egi.eu/sw/production/cas/1/current/\nenabled=1\ngpgcheck=1\ngpgkey=http://repository.egi.eu/sw/production/cas/1/GPG-KEY-EUGridPMA-RPM-3" >> /etc/yum.repos.d/EGI-trustanchors.repo
 
+# install IGTF trust bundle
 yum -y install ca-policy-egi-core
 
+# make dir for ssl keys
 mkdir -p /etc/httpd/ssl
 
+# create self signed certificates
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/httpd/ssl/apache.key -out /etc/httpd/ssl/apache.crt -subj "/C=GB/ST=Example/L=Example/O=Example/OU=Example/CN=$(hostname).$(domainname)"
 
+# start apache
 service httpd start
