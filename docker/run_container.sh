27f6cc9393b78a54968b9292c30d50dd81cdf01b
Greg Corbett <greg.corbett@stfc.ac.uk>
2016-05-20 16:35:47 +0100

    Move docker scripts to docker folder

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+# This may or may not work as script, but these are the commands to run
+
 echo "This script will deploy the containers for INDIGO-DataCloud Accounting."
 
+docker pull gregcorbett/rest
 
 echo "Configuring Database"
 MYSQL_ROOT_PASSWORD="EagerOmega"
 
 docker run -v /var/lib/mysql --name apel-mysql -p 3306:3306 -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" -d mysql
 
+#wait for apel-mysql to configure
+sleep 30
+
 docker exec apel-mysql mysql -u root -p$MYSQL_ROOT_PASSWORD -e "create database apel_rest"
-docker exec apel-mysql mysql -u root -p$MYSQL_ROOT_PASSWORD -e apel_rest < schemas/cloud.sql
+docker exec apel-mysql mysql -u root -p$MYSQL_ROOT_PASSWORD apel_rest -e "`cat schemas/cloud.sql`"
 
 echo "Done"
 
@@ -17,4 +23,4 @@ HOST_NAME=$(hostname)
 echo "Creating (self-signed) cert of $HOST_NAME"
 
 echo "Configuring APEL Server"
-docker run -it --link apel-mysql:mysql -p 80:80 -p 443:443 -e "HOST_NAME=$HOST_NAME" -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" gregcorbett/rest
+docker run -d --link apel-mysql:mysql -p 80:80 -p 443:443 -e "HOST_NAME=$HOST_NAME" -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" gregcorbett/rest
