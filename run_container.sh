df326dd031d3214561a446a04e48b0e2cfad0bbe
Greg Corbett <greg.corbett@stfc.ac.uk>
2016-05-20 12:07:12 +0100

    Configures persistent mysql storage

--- run_container.sh
+++ run_container.sh
@@ -2,10 +2,15 @@
 
 echo "This script will deploy the containers for INDIGO-DataCloud Accounting."
 
-docker run -d -v /var/lib/mysql --name apel-mysql --entrypoint /bin/echo mysql
+MYSQL_ROOT_PASSWORD="EagerOmega"
+
+docker run -v /var/lib/mysql --name apel-mysql -p 3306:3306 -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" -d mysql
 
 HOST_NAME=$(hostname)
 
 echo "Creating (self-signed) cert of $HOST_NAME"
 
-docker run -d --volumes-from apel-mysql -p 80:80 -p 443:443 -e "HOST_NAME=$HOST_NAME" gregcorbett/rest
+docker run -it --link apel-mysql:mysql -p 80:80 -p 443:443 -e "HOST_NAME=$HOST_NAME" gregcorbett/rest
+
+# this is the mysql command to run from inside linked container
+# mysql -h $MYSQL_PORT_3306_TCP_ADDR -u root -p
