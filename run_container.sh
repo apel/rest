147bd92826da8a10d49e0aab318d16e028750ff4
Greg Corbett <greg.corbett@stfc.ac.uk>
2016-05-20 11:05:20 +0100

    Add mysql volumes for data storage

--- run_container.sh
+++ run_container.sh
@@ -2,8 +2,10 @@
 
 echo "This script will deploy the containers for INDIGO-DataCloud Accounting."
 
+docker run -d -v /var/lib/mysql --name apel-mysql --entrypoint /bin/echo mysql
+
 HOST_NAME=$(hostname)
 
 echo "Creating (self-signed) cert of $HOST_NAME"
 
-docker run -d -p 80:80 -p 443:443 -e "HOST_NAME=$HOST_NAME" gregcorbett/rest
+docker run -d --volumes-from apel-mysql -p 80:80 -p 443:443 -e "HOST_NAME=$HOST_NAME" gregcorbett/rest
