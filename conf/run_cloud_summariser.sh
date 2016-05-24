f7271ab96d40341b2c0a9180d54e03615938f98a
Greg Corbett <greg.corbett@stfc.ac.uk>
2016-05-24 14:19:20 +0100

    Run summariser as root

--- conf/run_cloud_summariser.sh
+++ conf/run_cloud_summariser.sh
@@ -7,7 +7,7 @@
 sleep 5
 
 # Run the summariser
-su apel -c 'python /usr/bin/apelsummariser -d /etc/apel/clouddb.cfg -c /etc/apel/cloudsummariser.cfg'
+python /usr/bin/apelsummariser -d /etc/apel/clouddb.cfg -c /etc/apel/cloudsummariser.cfg
 
 # Wait to ensure summariser has stopped
 sleep 5
