d0f2200887ef0f1c59cde9026dc2cbf214fdcc65
Greg Corbett <greg.corbett@stfc.ac.uk>
2016-05-25 16:56:55 +0100

    Seperate GET and POST endpoints

--- api/views/CloudRecordSummaryView.py
+++ api/views/CloudRecordSummaryView.py
@@ -29,11 +29,6 @@ class CloudRecordSummaryView(APIView):
 
     .../computing/summaryrecord?&from=<date_from>
     Will give summary for whole infrastructure from <data> to now
-
-    POST Usage:
-    .../computing/summaryrecord
-
-    Will save Cloud Accounting Records for later loading.
     """
 
     def get(self, request, format=None):
@@ -114,66 +109,6 @@ class CloudRecordSummaryView(APIView):
         results = self._filter_cursor(cursor)
         results = self._paginate_result(request, results)
         return Response(results, status=200)
-
-    def post(self, request, format=None):
-        """
-        Submit Cloud Accounting Records.
-
-        .../computing/summaryrecord
-
-        Will save Cloud Accounting Records for later loading.
-        """
-        logger = logging.getLogger(__name__)
-
-        try:
-            empaid = request.META['HTTP_EMPA_ID']
-        except KeyError:
-            empaid = 'noid'
-
-        logger.info("Received message. ID = %s", empaid)
-
-        try:
-            signer = request.META['SSL_CLIENT_S_DN']
-        except KeyError:
-            signer = "None"
-
-        if "_content" in request.POST.dict():
-            # then POST likely to come via the rest api framework
-            # hence use the content of request.POST as message
-            body = request.POST.get('_content')
-
-        else:
-            # POST likely to comes through a browser client or curl
-            # hence use request.body as message
-            body = request.body
-
-        logger.debug("Message body received: %s", body)
-
-        for header in request.META:
-            logger.debug("%s: %s", header, request.META[header])
-
-        # taken from ssm2
-        QSCHEMA = {'body': 'string',
-                   'signer': 'string',
-                   'empaid': 'string?'}
-
-        inqpath = os.path.join(settings.QPATH, 'incoming')
-        inq = Queue(inqpath, schema=QSCHEMA)
-
-        try:
-            name = inq.add({'body': body,
-                            'signer': signer,
-                            'empaid': empaid})
-        except QueueError as err:
-            logger.error("Could not save %s/%s: %s", inqpath, name, err)
-
-            response = "Data could not be saved to disk, please try again."
-            return Response(response, status=500)
-
-        logger.info("Message saved to in queue as %s/%s", inqpath, name)
-
-        response = "Data successfully saved for future loading."
-        return Response(response, status=202)
 
 ###############################################################################
 #                                                                             #
