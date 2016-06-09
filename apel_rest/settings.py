79de89dd815175c9389de3fa2249767f91f84f48
Greg Corbett <greg.corbett@stfc.ac.uk>
2016-06-09 11:53:38 +0100

    Sensitive settings removed

--- apel_rest/settings.py
+++ apel_rest/settings.py
@@ -166,6 +166,3 @@ RETURN_HEADERS = ["VOGroup",
 # this doesnt do anything, probably because of using older Django
 URL_FORMAT_OVERRIDE = None
 
-ALLOWED_FOR_GET=os.environ["ALLOWED_FOR_GET"]
-SERVER_ID=os.environ["SERVER_IAM_ID"]
-SERVER_SECRET=os.environ["SEVER_IAM_SECRET"]
