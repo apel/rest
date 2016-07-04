fc5daed7cdb27ed72c6e4f23e9ba432d6b4fc634
Greg Corbett <greg.corbett@stfc.ac.uk>
2016-07-04 10:13:57 +0100

    Extend test case for parsing query parameters
    - now check defaults for Group, Service, FromDate
    - improves pep8/pylint/pep257 compliance

--- api/tests/test_cloud_record_summary.py
+++ api/tests/test_cloud_record_summary.py
@@ -3,35 +3,37 @@ This module tests GET and POST requests.
 to the Cloud Sumamry Record endpoint.
 """
 
-import glob
 import logging
-import os
-import shutil
 
 from api.views.CloudRecordSummaryView import CloudRecordSummaryView
-from django.test import Client, TestCase
-from rest_framework.request import Request
+from django.test import TestCase
 from rest_framework.test import APIRequestFactory
 
 QPATH_TEST = '/tmp/django-test/'
 
 
 class CloudRecordSummaryTest(TestCase):
-    """
-    Tests GET requests to the Cloud Sumamry Record endpoint.
-    """
+    """Tests GET requests to the Cloud Sumamry Record endpoint."""
+
     def setUp(self):
         """Disable logging.INFO from appearing in test output."""
         logging.disable(logging.INFO)
 
     def test_parse_query_parameters(self):
-        test_cloud_view = CloudRecordSummaryView() 
+        """Test the parsing of query parameters."""
+        # test a get with group, summary, start and end
+        test_cloud_view = CloudRecordSummaryView()
         factory = APIRequestFactory()
         request = factory.get('/api/v1/cloud/record/summary?group=Group1&service=Service1&from=FromDate&to=ToDate', {})
-
         parsed_responses = test_cloud_view._parse_query_parameters(request)
         self.assertEqual(parsed_responses,
                          ("Group1", "Service1", "FromDate", "ToDate"))
+
+        # test a get with just an end date
+        request = factory.get('/api/v1/cloud/record/summary?to=ToDate', {})
+        parsed_responses = test_cloud_view._parse_query_parameters(request)
+        self.assertEqual(parsed_responses,
+                         (None, None, None, "ToDate"))
 
     def test_paginate_result(self):
         """Test an empty result is paginated correctly."""
@@ -45,11 +47,11 @@ class CloudRecordSummaryTest(TestCase):
         self.assertEqual(content, expected_content)
 
     def test_request_to_token(self):
-        """Test a token can be extracted from request"""
+        """Test a token can be extracted from request."""
         test_cloud_view = CloudRecordSummaryView()
         factory = APIRequestFactory()
-        request = factory.get('/api/v1/cloud/record/summary?from=FromDate', 
-                              HTTP_AUTHORIZATION = 'Bearer ThisIsAToken')
+        request = factory.get('/api/v1/cloud/record/summary?from=FromDate',
+                              HTTP_AUTHORIZATION='Bearer ThisIsAToken')
 
         token = test_cloud_view._request_to_token(request)
         self.assertEqual(token, 'ThisIsAToken')
@@ -62,23 +64,19 @@ class CloudRecordSummaryTest(TestCase):
 
         self.assertRaises(KeyError, test_cloud_view._request_to_token, request)
 
-
     def test_is_client_authorized(self):
         """Test a example client is authorised."""
         test_cloud_view = CloudRecordSummaryView()
-        factory = APIRequestFactory()
         with self.settings(ALLOWED_FOR_GET='IAmAllowed'):
             self.assertTrue(test_cloud_view._is_client_authorized('IAmAllowed'))
 
     def test_is_client_authorized_fail(self):
         """Test the failure of un-authorised clients."""
         test_cloud_view = CloudRecordSummaryView()
-        factory = APIRequestFactory()
         with self.settings(ALLOWED_FOR_GET='IAmAllowed'):
             self.assertFalse(test_cloud_view._is_client_authorized('IAmNotAllowed'))
 
-
-    #def test_filter_cursor(self):
+    # def test_filter_cursor(self):
     #    pass
 
     def tearDown(self):
