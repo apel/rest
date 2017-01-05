aac37f834d6881c4b66888987ae3655fe380fb61
Greg Corbett <greg.corbett@stfc.ac.uk>
2017-01-05 13:45:36 +0000

    Add (LRVM) SummariseVMs() to update_schema.sql
    - copied from schemas/cloud.sql, as the summariser
      for Indigo is different than that of the main
      APEL repository

--- scripts/update_schema.sql
+++ scripts/update_schema.sql
@@ -195,30 +195,115 @@ DROP PROCEDURE IF EXISTS SummariseVMs;
 DELIMITER //
 CREATE PROCEDURE SummariseVMs()
 BEGIN
-    REPLACE INTO CloudSummaries(SiteID, CloudComputeServiceID, Day, Month, Year, GlobalUserNameID, VOID,
-        VOGroupID, VORoleID, Status, CloudType, ImageId, EarliestStartTime, LatestStartTime,
-        WallDuration, CpuDuration, CpuCount, NetworkInbound, NetworkOutbound, Memory, Disk,
+CREATE TEMPORARY TABLE TCloudRecordsWithMeasurementTime
+(INDEX index_measurementtime USING BTREE (MeasurementTime))
+SELECT *, TIMESTAMPADD(SECOND, (IFNULL(SuspendDuration, 0) + WallDuration), StartTime) as MeasurementTime FROM CloudRecords;
+
+CREATE TEMPORARY TABLE TGreatestMeasurementTimePerDay
+(INDEX index_greatestmeasurementtime USING BTREE (MaxMT))
+select
+        Year(MeasurementTime) as Year,
+        Month(MeasurementTime) as Month,
+        Day(MeasurementTime) as Day,
+        VMUUID,
+        max(MeasurementTime) as MaxMT
+        from TCloudRecordsWithMeasurementTime
+        group by
+                Year(MeasurementTime),
+                Month(MeasurementTime),
+                Day(MeasurementTime),
+                VMUUID
+;
+DROP TABLE IF EXISTS LastCloudRecordPerDay;
+CREATE TABLE LastCloudRecordPerDay
+(INDEX index_vmuuidyearmonthday USING BTREE (VMUUID, Year, Month, Day))
+SELECT
+        a.*,
+        Year(a.MeasurementTime) as Year,
+        Month(a.MeasurementTime) as Month,
+        Day(a.MeasurementTime) as Day
+        from TCloudRecordsWithMeasurementTime as a
+        left join
+        TGreatestMeasurementTimePerDay as b
+        on (
+                Year(a.MeasurementTime) = b.Year and
+                Month(a.MeasurementTime) = b.Month and
+                Day(a.MeasurementTime) = b.Day and
+                a.VMUUID = b.VMUUID
+        )
+        where a.MeasurementTime = b.MaxMT
+
+        ORDER BY a.VMUUID, Year(a.MeasurementTime), Month(a.MeasurementTime), Day(a.MeasurementTime)
+;
+
+-- Based on discussion here: http://stackoverflow.com/questions/13196190/mysql-subtracting-value-from-previous-row-group-by
+
+CREATE TEMPORARY TABLE TVMUsagePerDay
+(INDEX index_VMUsagePerDay USING BTREE (VMUUID, Day, Month, Year))
+SELECT
+        ThisRecord.VMUUID as VMUUID,
+        ThisRecord.SiteID as SiteID,
+        ThisRecord.CloudComputeServiceID as CloudComputeServiceID,
+        ThisRecord.Day as Day,
+        ThisRecord.Month as Month,
+        ThisRecord.Year as Year,
+        ThisRecord.GlobalUserNameID as GlobalUserNameID,
+        ThisRecord.VOID as VOID,
+        ThisRecord.VOGroupID as VOGroupID,
+        ThisRecord.VORoleID as VORoleID,
+        ThisRecord.Status as Status,
+        ThisRecord.CloudType as CloudType,
+        ThisRecord.ImageId as ImageId,
+        ThisRecord.StartTime as StartTime,
+        COALESCE(ThisRecord.WallDuration - IFNULL(PrevRecord.WallDuration, 0.00)) AS ComputedWallDuration,
+        COALESCE(ThisRecord.CpuDuration - IFNULL(PrevRecord.CpuDuration, 0.00)) AS ComputedCpuDuration,
+        ThisRecord.CpuCount as CpuCount,
+        COALESCE(ThisRecord.NetworkInbound - IFNULL(PrevRecord.NetworkInbound, 0.00)) AS ComputedNetworkInbound,
+        COALESCE(ThisRecord.NetworkOutbound - IFNULL(PrevRecord.NetworkOutbound, 0.00)) AS ComputedNetworkOutbound,
+        ThisRecord.PublicIPCount as PublicIPCount,
+        -- Will Memory change during the course of the VM lifetime? If so, do we report a maximum, or
+        -- average, or something else?
+        -- If it doesn't change:
+        ThisRecord.Memory,
+        ThisRecord.Disk, -- As above: constant or changing?
+        ThisRecord.BenchmarkType as BenchmarkType,
+        ThisRecord.Benchmark as Benchmark
+FROM    LastCloudRecordPerDay as ThisRecord
+LEFT JOIN LastCloudRecordPerDay as PrevRecord
+ON      (ThisRecord.VMUUID = PrevRecord.VMUUID and
+        PrevRecord.MeasurementTime = (SELECT max(MeasurementTime)
+                                        FROM LastCloudRecordPerDay
+                                        WHERE VMUUID = ThisRecord.VMUUID
+                                        AND MeasurementTime < ThisRecord.MeasurementTime)
+        );
+
+REPLACE INTO CloudSummaries(SiteID, CloudComputeServiceID, Day, Month, Year,
+        GlobalUserNameID, VOID, VOGroupID, VORoleID, Status, CloudType, ImageId,
+        EarliestStartTime, LatestStartTime, WallDuration, CpuDuration, CpuCount,
+        NetworkInbound, NetworkOutbound, Memory, Disk,
         BenchmarkType, Benchmark, NumberOfVMs, PublisherDNID)
     SELECT SiteID,
-        CloudComputeServiceID,
-        DAY(StartTime) As Day, MONTH(StartTime) AS Month, YEAR(StartTime) AS Year,
-        GlobalUserNameID, VOID, VOGroupID, VORoleID, Status, CloudType, ImageId,
-        MIN(StartTime),
-        MAX(StartTime),
-        SUM(WallDuration),
-        SUM(CpuDuration),
-        CpuCount,
-        SUM(NetworkInbound),
-        SUM(NetworkOutbound),
-        SUM(Memory),
-        SUM(Disk),
-        BenchmarkType,
-        Benchmark,
-        COUNT(*),
-        'summariser'
-    FROM CloudRecords
-    GROUP BY SiteID, CloudComputeServiceID, Day, Month, Year, GlobalUserNameID, VOID, VOGroupID, VORoleID,
-        Status, CloudType, ImageId, CpuCount, BenchmarkType, Benchmark
+    CloudComputeServiceID,
+    Day, Month, Year,
+    GlobalUserNameID, VOID, VOGroupID, VORoleID, Status, CloudType, ImageId,
+    MIN(StartTime),
+    MAX(StartTime),
+    SUM(ComputedWallDuration),
+    SUM(ComputedCpuDuration),
+    CpuCount,
+    SUM(ComputedNetworkInbound),
+    SUM(ComputedNetworkOutbound),
+    SUM(PublicIPCount),
+    SUM(Memory),
+    SUM(Disk),
+    BenchmarkType,
+    Benchmark,
+    COUNT(*),
+    'summariser'
+    FROM TVMUsagePerDay
+    GROUP BY SiteID, CloudComputeServiceID, Day, Month, Year, GlobalUserNameID, VOID,
+        VOGroupID, VORoleID, Status, CloudType, ImageId, CpuCount,
+        BenchmarkType, Benchmark
     ORDER BY NULL;
 END //
 DELIMITER ;
