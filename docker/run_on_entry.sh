9cce557729acba22395b52b3d2ce2a9458d5cbff
Greg Corbett <greg.corbett@stfc.ac.uk>
2017-09-06 10:48:00 +0100

    Fix errors caused by systemd not being present
    - can't start 'fetch-crl-cron' service so need to modify the cron
      to not check if that service is running
    - use bin files to start httpd, atd and crond

--- docker/run_on_entry.sh
+++ docker/run_on_entry.sh
@@ -28,17 +28,21 @@ sed -i "s|\['allowed_for_get'\]|$ALLOWED_FOR_GET|g" /var/www/html/apel_rest/sett
 
 # fetch the crl first
 fetch-crl
-# then start the periodic fetch-url
-service fetch-crl-cron start
+
+# alter the fetch-crl cron to run regardless of any services
+echo "# Cron job running by default every 6 hours, at 45 minutes past the hour
+# with  +/- 3 minutes sleep.
+
+45 */6 * * * root /usr/sbin/fetch-crl -q -r 360" >  /etc/cron.d/fetch-crl
 
 # start apache
-service httpd start
+/usr/sbin/httpd
 
 # start cron
-service crond start
+/usr/sbin/crond
 
 # start at
-service atd start
+/usr/sbin/atd
 
 # install IGTF trust bundle 10 minutes after start up
 echo "yum -y update ca-policy-egi-core >> /var/log/IGTF-startup-update.log" | at now + 10 min
